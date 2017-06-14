3a7e26673f761d6f0324a8c5dc7a7e987c143141
james <jtronlabs@gmail.com>
2017-06-14 15:33:45 -0400

    stuff is moving fast! however cannot remove geo waypoints

--- source/javascripts/find.js
+++ source/javascripts/find.js
@@ -1,28 +1,46 @@
 var map;
 var ac_start, ac_end;
+var dirService, dirRenderer;
 
 async function initialize() {
     //init autocomplete
     let start = document.getElementById('start');
+    ac_start = new google.maps.places.Autocomplete(start);
     let end = document.getElementById('end');
-    ac_start = new google.maps.places.Autocomplete(end);
-    ac_end = new google.maps.places.Autocomplete(start);
-    google.maps.event.addListener(ac_start, 'place_changed', modStartPos);
-    google.maps.event.addListener(ac_end, 'place_changed', modEndPos);
+    ac_end = new google.maps.places.Autocomplete(end);
 
-    //get start pos
+    //init map with options
     var pos = await getLocation();
-    pos.coords['lat'] = pos.coords['latitude'];
-    pos.coords['lng'] = pos.coords['longitude'];
-
-    //init map
+    var zoom = 16;
+    if (pos) {
+        pos.coords['lat'] = pos.coords['latitude'];
+        pos.coords['lng'] = pos.coords['longitude'];
+    } else {
+        pos = {
+            'coords': {
+                'lat': 0,
+                'lng': 0
+            }
+        };
+        zoom = 3;
+    }
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 16,
+        zoom: zoom,
         center: pos.coords
+    });
+
+    //init directions
+    dirService = new google.maps.DirectionsService();
+    dirRenderer = new google.maps.DirectionsRenderer({
+        draggable: true,
+        map: map,
+        panel: document.getElementById('renderer-panel')
     });
 
     //add google event listeners
     google.maps.event.addDomListener(window, "resize", resizeMap);
+    google.maps.event.addListener(ac_start, 'place_changed', modStartPos);
+    google.maps.event.addListener(ac_end, 'place_changed', modDestPos);
 }
 
 async function getLocation() {
@@ -36,24 +54,15 @@ async function getLocation() {
 
                     resolve(pos)
                 },
-                promptForCoords,
+                null,
                 {
                     enableHighAccuracy: true,
                     timeout: 4000,
                     maximumAge: 0
                 });
         } else {
-            resolve(promptForCoords());
+            resolve(null);
         }
-    });
-}
-
-async function promptForCoords() {
-    return new Promise((resolve, reject) => {
-        let address = "";
-        let pos = null;
-
-        resolve(pos);
     });
 }
 
@@ -63,9 +72,42 @@ function resizeMap() {
     map.setCenter(center);
 }
 
-function modStartPos(){
-    console.log(ac_start.getPlace());
+function getStart() {
+    return ac_start.getPlace();
 }
-function modEndPos(){
-    console.log(ac_end.getPlace());   
+function getDest() {
+    return ac_end.getPlace();
+}
+
+function modStartPos() {
+    let pos = getStart().geometry.location;
+    if (pos) {
+        map.setCenter(pos);
+
+        //change map bounds
+        map.fitBounds(getStart().geometry.viewport);
+    }
+}
+function modDestPos() {
+    console.log(getDest());
+}
+
+function getDirs() {
+    dirService.route(
+        {
+            origin: getStart().geometry.location,
+            destination: getDest().geometry.location,
+            travelMode: 'DRIVING',
+            unitSystem: google.maps.UnitSystem.IMPERIAL,
+            // provideRouteAlternatives: true,
+            avoidTolls: true
+        },
+        function (result, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                dirRenderer.setDirections(result);       
+            }else if (status == google.maps.DirectionsStatus.ZERO_RESULTS){
+                alert("no results");
+            }
+            console.log(result,status);
+        });
 }
