1024906ffdc9f758932f51a386c88ae3a568ecf0
james <jtronlabs@gmail.com>
2017-06-14 19:31:55 -0400

    searches were working but getting them to return into one array via promise.all is causing issues.

--- source/javascripts/find.js
+++ source/javascripts/find.js
@@ -1,8 +1,16 @@
 var map;
 var ac_start, ac_end;
+var placeService;
 var dirService, dirRenderer;
+var rb, search_dist_from_route = .5;
+
+
+document.addEventListener("DOMContentLoaded", initialize);
+
 
 async function initialize() {
+    rb = new RouteBoxer();
+
     //init autocomplete
     let start = document.getElementById('start');
     ac_start = new google.maps.places.Autocomplete(start);
@@ -28,6 +36,9 @@ async function initialize() {
         zoom: zoom,
         center: pos.coords
     });
+
+    //init places
+    placeService = new google.maps.places.PlacesService(map);
 
     //init directions
     dirService = new google.maps.DirectionsService();
@@ -104,10 +115,71 @@ function getDirs() {
         },
         function (result, status) {
             if (status == google.maps.DirectionsStatus.OK) {
-                dirRenderer.setDirections(result);       
-            }else if (status == google.maps.DirectionsStatus.ZERO_RESULTS){
+                dirRenderer.setDirections(result);
+            } else if (status == google.maps.DirectionsStatus.ZERO_RESULTS) {
                 alert("no results");
             }
-            console.log(result,status);
+            console.log(result, status);
         });
+}
+
+async function findGas() {
+    let path = dirRenderer.getDirections().routes[0].overview_path;
+    bounds = rb.box(path, search_dist_from_route);
+    console.log(await searchBounds(bounds));
+}
+
+
+/*
+* Source: https://www.cookieshq.co.uk/posts/search-places-along-a-route-with-google-maps-and-routboxer
+* The search function is wrap around a for loop with a setTimeout so that the request are throttled to an 
+* avg of 10 a second that is the max request you can make on google
+*/
+
+async function searchBounds(bnds) {
+    let promises = [];
+
+    //perform searches inside the passed-in bounds to find places near the route
+    for (let i = 0; i < bnds.length; i++) {
+        //timeout to ensure not too many at once
+        setTimeout(() => {
+            // Perform search on the bound and save the result
+            // https://developers.google.com/maps/documentation/javascript/places#radar_search_requests
+            let search = new Promise((resolve, reject) => {
+                placeService.radarSearch({
+                    bounds: bnds[i],
+                    type: 'gas_station'
+                }, (results, status) => { //callback for search result
+                    if (status === google.maps.places.PlacesServiceStatus.OK) {
+                        resolve(results);
+                    } else {
+                        reject(status);
+                    }
+                });
+            });
+
+            promises.push(search)
+        }, 400 * i);
+    }
+
+    //wait for all the searches to finish
+    console.log("timeout ended", promises)
+    return Promise.all(promises).then(function(val){
+        console.log(val)
+    }
+    //     searches_result => {
+    //     console.log(searches_result)
+    //     let unique_places = {};
+    //     for (let search_results in searches_result) {
+    //         //remove duplicate places
+    //         for (let result in search_results) {
+    //             console.log(result);
+    //             if (!(result.place_id in establishments)) {
+    //                 unique_places[result.place_id] = "";
+    //             }
+    //         }
+    //     }
+    //     return searches_result;
+    // }
+    );
 }
